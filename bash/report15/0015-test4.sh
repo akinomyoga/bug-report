1b84d6279d6c77c8389ab7efd4c5399706d380af
Koichi Murase <myoga.murase@gmail.com>
2020-04-19 21:20:19 +0900

    bash/report15: update test4.sh

--- bash/report15/0015-test4.sh
+++ bash/report15/0015-test4.sh
@@ -17,7 +17,21 @@ handler() { setexit 111; return; }
 trap 'handler; stat=$?; return' USR1
 invoke
 case $stat in
-(0)   echo 'In function call: last command preceding the trap action' ;;
-(111) echo 'In function call: last command in the trap action' ;;
-(*)   echo 'In function call: (unexpected)' ;;
+(0)   echo 'In direct function call: last command preceding the trap action' ;;
+(111) echo 'In direct function call: last command in the trap action' ;;
+(*)   echo 'In direct function call: (unexpected)' ;;
 esac
+
+stat=99
+utility2() { setexit 111; return; }
+handler2() { utility2; stat=$?; }
+trap 'handler2' USR1
+invoke
+case $stat in
+(0)   echo 'In indirect function call: last command preceding the trap action' ;;
+(111) echo 'In indirect function call: last command in the trap action' ;;
+(*)   echo 'In indirect function call: (unexpected)' ;;
+esac
+
+trap 'false && echo ERROR || echo OK' USR1
+invoke
