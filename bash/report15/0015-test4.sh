a34f9dcbae846a0e3ebdcbb508ff01b7b3ec1210
Koichi Murase <myoga.murase@gmail.com>
2020-05-12 16:07:58 +0900

    bash/report15: update mail.txt

--- bash/report15/0015-test4.sh
+++ bash/report15/0015-test4.sh
@@ -6,10 +6,10 @@ invoke() { kill -USR1 $$; return 222; }
 trap 'setexit 111; return' USR1
 invoke
 case $? in
-(0)   echo 'In trap argument: last command preceding the trap action' ;;
-(111) echo 'In trap argument: last command in the trap action' ;;
-(222) echo 'In trap argument: (failed to exit the function)' ;;
-(*)   echo 'In trap argument: (unexpected)' ;;
+0)   echo 'In trap argument: last command preceding the trap action' ;;
+111) echo 'In trap argument: last command in the trap action' ;;
+222) echo 'In trap argument: (failed to exit the function)' ;;
+*)   echo 'In trap argument: (unexpected)' ;;
 esac
 
 stat=99
@@ -17,9 +17,9 @@ handler() { setexit 111; return; }
 trap 'handler; stat=$?; return' USR1
 invoke
 case $stat in
-(0)   echo 'In direct function call: last command preceding the trap action' ;;
-(111) echo 'In direct function call: last command in the trap action' ;;
-(*)   echo 'In direct function call: (unexpected)' ;;
+0)   echo 'In direct function call: last command preceding the trap action' ;;
+111) echo 'In direct function call: last command in the trap action' ;;
+*)   echo 'In direct function call: (unexpected)' ;;
 esac
 
 stat=99
@@ -28,9 +28,9 @@ handler2() { utility2; stat=$?; }
 trap 'handler2' USR1
 invoke
 case $stat in
-(0)   echo 'In indirect function call: last command preceding the trap action' ;;
-(111) echo 'In indirect function call: last command in the trap action' ;;
-(*)   echo 'In indirect function call: (unexpected)' ;;
+0)   echo 'In indirect function call: last command preceding the trap action' ;;
+111) echo 'In indirect function call: last command in the trap action' ;;
+*)   echo 'In indirect function call: (unexpected)' ;;
 esac
 
 trap 'false && echo ERROR || echo OK' USR1
