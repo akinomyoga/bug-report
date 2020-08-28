5fd5201b0bf1ed8e22584fcf95dc689062802c02
Koichi Murase <myoga.murase@gmail.com>
2020-08-28 09:24:46 +0900

    bash: add report18

---
+++ bash/report18/2017-05-msg00076.generate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+{
+  echo -n Zm9yICgoMGZ1bmOOaW9uOykpOyBkbyA6vuHSvr5lJgpkfwBlIDo7IGRvbmUm92RcXFxjciRcb3Ph
+  echo -n Y1xc4WNcXF5cXN1cXlxc3VxcXGNyXF5cXN1cXFxjJ/8a4QAQXF7cU91cXFxcXA9cSFxcXFzFc+Fj
+  echo -n XFzhY1xcXlxc3VxeXFzdXFxcY3JcXlxc3VxcXGMn/xrhABBcXtxc3VxcXFxcD1xIXFxcXFwPXEhc
+} | base64 -d > 2017-05-msg00076.sh
+
---
+++ bash/report18/test18.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo Complete begin
+eval '{ echo; }'
+echo Complete end
+
+echo Incomplete begin
+eval {
+echo Incomplete end
+#{
