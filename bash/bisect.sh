faf215efcf83efaf3bbe63ac0f41523b3e7ca7e4
Koichi Murase <myoga.murase@gmail.com>
2023-02-21 22:40:31 +0900

    bash: update bisect.sh

--- bash/bisect.sh
+++ bash/bisect.sh
@@ -7,23 +7,43 @@
 
 #git bisect start f9f8a7fa 3e03eaff
 
+mkd() { [[ -d $1 ]] || mkdir -p "$1"; }
+
 rebuild() {
-  # Build
-  make -j clean
-  make distclean
-  git reset --hard
-  sed -i 's/define(relstatus, .*)/define(relstatus, release)/' configure.ac
-  autoreconf -i
-  ./configure
-  make -j all || make -j all
-  make all || make all
-  rm -f parser-built y.tab.c y.tab.h
-  git reset --hard
+  local -x hash=$(git rev-parse HEAD)
+  local cache=bisect-bin/bash.$hash
+  if [[ -s $cache && -x $cache ]]; then
+    cp "$cache" ./bash || return 1
+  else
+    mkd bisect-bin || return 1
+
+    (
+      # Clean up
+      rm -f ./bash
+      make -j clean
+      make distclean
+
+      # Build
+      git reset --hard &&
+        { [[ $1 == no-release ]] ||
+            sed -i 's/define(relstatus, .*)/define(relstatus, release)/' configure.ac; } &&
+        { autoreconf -i; ./configure; } &&
+        { make -j all || make -j all || make all || make all; } &&
+        rm -f parser-built y.tab.c y.tab.h &&
+        git reset --hard || return 1
+
+      # Cache binary
+      [[ -s bash && -x bash ]] &&
+        cp ./bash "$cache" &&
+        touch --date="$(git show -s --format=%ci @)" "$cache" &&
+        "$cache" -c 'echo "bash.$hash $BASH_VERSION ($MACHTYPE)"' >> bisect-bin/versions || return 1
+    ) >> "$cache.log" 2>&1
+  fi
 }
 
 case $1 in
 (rebuild)
-  rebuild ;;
+  rebuild || rebuild no-release ;;
 (test)
   if [[ $3 ]]; then
     if ! git checkout "$3"; then
@@ -40,7 +60,7 @@ case $1 in
   _bisect_command=$2 git bisect run ./bisect.sh ;;
 (*)
   if [[ $_bisect_command ]]; then
-    rebuild
+    rebuild || rebuild no-release || exit 125
     eval "$_bisect_command"
   fi ;;
 esac
@@ -49,3 +69,4 @@ esac
 #[[ $lastline == 'ok 3000' ]]
 #./bash bisect1.sh | grep -q false
 
+# ./bisect.sh start ./bisect3.sh 407d9afc 9b44e16c6f
